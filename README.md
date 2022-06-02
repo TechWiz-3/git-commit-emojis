b32b3122694d002aa55199fd62a8c874857d87fe
TechWiz-3 <programmingplus1@gmail.com>
2022-06-02 16:46:00 +1000

    🎉 INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,32 @@
+# Git Commit Emojis
+
+/**License badge
+python badge
+stars badge
+issues badge
+pull requests badge**/
+
+A simple cross platform python script that automates  easy labelling of git commits with descriptive emojis and text.
+
+## About
+We all like a bit of extra color and spice to our boring old commit messages, this not only adds that but also adds information about the commit.
+
+## Installation
+Since the project will continue to be updated and isn't on pypi, you can clone the repository and create a CLI command linking to the cloned repository
+
+## Usage
+
+## The Story
+So I noticed that [this](https://github.com/msaaddev) programmer's commits always had really neat looking labels with emojis. I searched it up and found a number of repositories or gists talking about emojis for various sorts of commits.  
+However, I couldn't find ANY projects which automated the experience. Weird, right? [One](https://github.com/ahmadawais/Emoji-Log) project had a way to somewhat automate the experience by adding a function for each commit type. While a nice solution, I don't want to have to memorise the abbreviations/commands type of commit. Well no more, with this project, I can now label all my commits in mere seconds without having to memorise anything.
+
+Here are the emoji guides mentioned above, they are all great and this project wouldn't be possible without them so be sure to check them out:  
+[Git Command Emoji Gist](https://gist.github.com/parmentf/035de27d6ed1dce0b36a)  
+[Emoji-Log](https://github.com/ahmadawais/Emoji-Log)  
+[Git Commit Emoji](https://github.com/liuchengxu/git-commit-emoji-cn)  
+
+
+## Label Guide
+### Select Menu Options
+
+### Shorcut Options:
---
+++ gc.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+# Zac the Wise on 24 May
+# Little script that adds emojis and labels for git commits
+# More info: https://github.com/TechWiz-3/git-commit-emojis 
+
+from sys import argv  # import cli arguement function
+from sys import exit  # import exit function
+from os import system
+
+def help():
+    """Help command"""
+    print(
+        "\n\n"\
+        "Git emoji/labeling tool created by Zac the Wise"\
+        "\n\n"\
+        "Basic Usage:",
+        "\n\n"\
+        "gc -m <commit message>"\
+        "\n\n"\
+        "      -h, --help      shows this command"\
+        "\n\n"\
+        "      -m <\"message\">     commit message (required), use quotation marks"\
+        "\n\n"\
+        "Advanced Usage:"\
+        "\n\n"\
+        "The commit option can be omitted for shortcut commit messages:"\
+        "\n\n"\
+        "       -sh <shortcut>      puts the commit message for you as the value of 'shortcut'"\
+        "\n\n"\
+        "Shortcuts:\n\nty     commit message defaults to: ✏️ FIX TYPO\n\ncl
+        commit message defaults to: 🧹 CLEAN UP\n\nin       commit message
+        defaults to: 🎉 INITIAL COMMIT\n"\
+        "\n\n"
+    )
+
+def get_opts():
+    """Get cli options/arguements"""
+    for arg in argv[1:]:  # gets arguements except the first one and assigns them
+        if arg == "-m":  # message option
+            commit_message = ""
+            # get the argument for the option
+            get_arg_index = argv.index('-m')  # find the index number for the option
+            try:
+                args_after_m = argv[get_arg_index+1:]
+                for msg_word in args_after_m:
+                    commit_message += f"{msg_word} "
+                commit_message = commit_message[:-1]  # remove extra space from end
+            except IndexError:
+                return "error", "You didn't include the arguement for the commit message like so: -m \"my commit message\""
+            else:
+                if commit_message == "":  # if the commit message is empty
+                    return "error", "Commit message cannot be empty"
+                return "message only", commit_message
+        elif arg == "-sh":
+            get_arg_index = argv.index('-sh')  # find the index number for the option
+            try:
+                shortcut = argv[get_arg_index+1]  # get the arguement after the option and assign it
+            except IndexError:
+                return "error", "You didn't include the arguement for the shortcut type: -sh <shortcut value>"
+            else:
+                if shortcut == "":  # if the shortcut value is empty
+                    return "error", "Shortcut value cannot be empty"
+                elif shortcut == "ty" or shortcut == "typo":
+                    commit_message = "✏️ FIX TYPO"
+                    return "shortcut", commit_message
+                elif shortcut == "cl" or shortcut == "clean":
+                    commit_message = "🧹 CLEAN UP"
+                    return "shortcut", commit_message
+                elif shortcut = "in" or shorcut = "init" or shortcut =
+                "initial":
+                    commit_message = "🎉 INITIAL COMMIT"
+                    return "shortcut", commit_message
+                else:
+                    return "error", "Unrecognized shortcut usage"
+        elif arg == "-h" or arg == "--help":  # help option
+            help()  # display help message
+            return exit()  # stop the rest of the program from running
+
+def select_menu():
+    """Displays the select menu and returns the value to enter into the commit"""
+    options = ["👌 Improvement", "📦 Addition", "📖 Documentation", "🐛 Bug-fix", "🔖 Version-tag", "🚪 Exit"]
+    commit_label = ""
+    for option in options:
+        opt_no = options.index(option)
+        print(f"{opt_no+1}) {option}")
+    select_label = int(input("Enter the number corresponding to which type of change you made: "))
+    if select_label == 1:
+        commit_label = "👌 IMPROVE: "
+    elif select_label == 2:
+        commit_label="📦 NEW: "
+    elif select_label == 3:
+        commit_label="📖 DOC: "
+    elif select_label == 4:
+        commit_label="🐛 FIX: "
+    elif select_label == 5:
+        commit_label="🔖 "
+    elif select_label == 6:
+        print("Exiting, have a nice day...")
+        exit()
+    return commit_label
+
+if __name__ == "__main__":
+
+    options = get_opts()
+    if options == None:
+        print("Incorrect usage. Refer to gc.py --help")
+    else:
+        typ, msg = options
+        if typ == "message only":
+            label = select_menu()
+            system(f"git commit -m \"{label}{msg}\"")
+        elif typ == "shortcut":
+            system(f"git commit -m \"{msg}\"")
+        elif typ == "error":
+            print(f"Error occured: {msg}")
